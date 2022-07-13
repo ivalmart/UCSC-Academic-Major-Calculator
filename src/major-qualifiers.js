6c45cb14f5ceacda10bcad6bf40cff29d6aa9201
Ivan Martinez-Arias <50786762+ivalmart@users.noreply.github.com>
2022-07-13 16:47:48 -0700

    Completed Program to Show Results

--- src/major-qualifiers.js
+++ src/major-qualifiers.js
@@ -4,29 +4,42 @@ let content_parsed = [];    // will contain an array of courses from the given l
 // main function
 function CalculateButton() {
     clearContentArray();
+    ClearMajorList();
 
-    // var content = document.getElementById("chosenList").textContent;
     var content = document.getElementById("chosenList").children;
 
-    if(content.length <= 0) console.log("N/A");   // if the list is empty, don't calculate anything
-    else parseContent(content);   // if there is at least 1 item in the list, we can calculate
-    
-    ReceiveMajorJSON();
-
-    // console.log(content_parsed);
+    if(content.length <= 0) {
+        console.log("N/A");   // if the list is empty, don't calculate anything
+    } else {
+        parseContent(content);   // if there is at least 1 item in the list, we can calculate
+        ReceiveMajorJSON();
+    }
 }
 
 // Calculates 
 // prop - major title
 // arr[prop] - all courses inside the major title
 let QualifyMajors = function() {
-    console.log("Inside Qualify Majors");
-    console.log(content_parsed);
-    // for(let prop in major_courseObj) {
-    //     console.log(prop);
-    //     console.log(major_courseObj[prop].length);
-    // };
-    console.log("End of Qualify Majors");
+    // 1st loop: goes through each major
+    for(let prop in major_courseObj) {
+        console.log(prop);
+        // 2nd loop: goes through each course in the major
+        for(let i = 0; i < major_courseObj[prop].length; i++) {
+            // 3rd loop: goes through the list of courses from the user to compare
+            let j = 0;
+            while(j < content_parsed.length) {
+                let course_Comp = major_courseObj[prop][i];
+                let user_course = content_parsed[j].slice(0, -1);
+                let result = user_course.localeCompare(course_Comp);
+
+                if(result == 0) {
+                    AddMajorToList(prop);
+                    break;
+                }
+                j++;
+            }
+        }
+    }
 }
 
 // takes in the given content list from the user and organizes it to access it easier
@@ -59,4 +72,19 @@ function ReceiveMajorJSON() {
 
 function clearContentArray() {
     while(content_parsed.length > 0) content_parsed.pop();
+}
+
+function AddMajorToList(major) {
+    console.log("ADDING MAJOR" + major);
+    var qualified = document.createElement("a");
+    qualified.textContent = major;
+  
+    var ul = document.getElementById("majorList");
+    var li = document.createElement("li");
+    li.appendChild(qualified);
+    ul.appendChild(li);
+}
+
+function ClearMajorList() {
+    document.getElementById("majorList").innerHTML = "";
 }
